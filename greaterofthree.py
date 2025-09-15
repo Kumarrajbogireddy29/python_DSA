d2017ca87a2e4148d556e925359f4831ae7f67e3
Kumar Raj Bogireddy <22MH1A42E4@acoe.edu.in>
2025-09-15 21:43:31 +0530

    First commit - uploaded all files

---
+++ greaterofthree.py
@@ -0,0 +1,11 @@
+num1=10
+num2=20
+num3=30
+if(num1>num2):
+    if(num1>num3):
+        print(num1,"is maximum")
+elif(num2>num3):
+    if(num2>num1):
+        print(num2,"is maximum")
+    else:
+        print(num3,"maximum")
---
+++ maxnumber.py
@@ -0,0 +1,14 @@
+num1=20
+num2=90
+if(num1>num2):
+    print(num1,"is maximum")
+else:
+    print(num2,"is maximum")
+
+#another method to find
+#max(num1,num2
+#print(max(num1,num2))
+
+
+#num1,num2=20,3)
+#print((num1 if num1>num2 else num2))
